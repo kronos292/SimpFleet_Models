7f03e7a9de1f056d8e3624b703a675fe8acca8dd
kronos292 <kronos292@users.noreply.github.com>
2019-07-01 21:16:49 +0800

    Update PSAVesselTracker.js

--- models/PSAVesselTracker.js
+++ models/PSAVesselTracker.js
@@ -3,9 +3,6 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
     psaBerthingData: {type: Schema.Types.ObjectId, ref: 'psaBerthingData'},
-    vesselLoadingLocation: {type: String, default: ''},
-    psaBerthingDateTime: {type: String, default: ''},
-    psaUnberthingDateTime: {type: String, default: ''},
     isEmail: {type: Boolean, default: false},
     isMobile: {type: Boolean, default: false},
     isActive: {type: Boolean, default: true}
