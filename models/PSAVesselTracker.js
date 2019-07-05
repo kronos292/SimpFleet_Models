1575867885bea6617bab288d3a2adb2a326a9919
kronos292 <kronos292@users.noreply.github.com>
2019-07-05 12:58:40 +0800

    added job reference to vessel tracker

--- models/PSAVesselTracker.js
+++ models/PSAVesselTracker.js
@@ -5,7 +5,8 @@ const schema = new Schema({
     psaVessel: {type: Schema.Types.ObjectId, ref: 'psaVessels'},
     isEmail: {type: Boolean, default: false},
     isMobile: {type: Boolean, default: false},
-    isActive: {type: Boolean, default: true}
+    isActive: {type: Boolean, default: true},
+    job: {type: Schema.Types.ObjectId, ref: 'jobs'}
 });
 
 module.exports = mongoose.model('psaVesselTrackers', schema);
