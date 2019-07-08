e87f7d85209107e46ebbfb1d5e40ffb2a60120b6
kronos292 <kronos292@users.noreply.github.com>
2019-07-08 12:23:03 +0800

    added sms variable in vessel tracker

--- models/VesselTracker.js
+++ models/VesselTracker.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const schema = new Schema({
     psaVessel: {type: Schema.Types.ObjectId, ref: 'psaVessels'},
     isEmail: {type: Boolean, default: false},
+    isSMS: {type: Boolean, default: false},
     isMobile: {type: Boolean, default: false},
     isActive: {type: Boolean, default: true},
     job: {type: Schema.Types.ObjectId, ref: 'jobs'}
