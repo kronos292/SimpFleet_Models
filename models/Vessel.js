2d5e6fd34f9224f1c35ef7db2d55f65883a7d09a
kronos292 <kronos292@users.noreply.github.com>
2019-07-11 12:49:20 +0800

    Update Vessel.js

--- models/Vessel.js
+++ models/Vessel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const vesselSchema = new Schema({
-    psaVessel: [{type: Schema.Types.ObjectId, ref: 'psaVessels'}],
+    psaVessels: [{type: Schema.Types.ObjectId, ref: 'psaVessels'}],
     mpaVessel: {type: Schema.Types.ObjectId, ref: 'mpaVessels'},
     vesselIMOID: {type: String, default: ""},
     vesselName: {type: String, default: ""},
