ac6367cff342f83bc9e4cc2439bc86c78855a86c
kronos292 <kronos292@users.noreply.github.com>
2019-07-16 21:23:59 +0800

    added job item pricing

--- models/JobItem.js
+++ models/JobItem.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const schema = new Schema({
     job: {type: Schema.Types.ObjectId, ref: 'jobs'},
     quantity: {type: Number, default: 1},
+    price: {type: Number, default: 0},
     uom: {type: String, default: ''}
 });
 
