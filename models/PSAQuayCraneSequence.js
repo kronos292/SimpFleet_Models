1e991e3f6b4c5c3bc26953a723f9b64100042137
kronos292 <kronos292@users.noreply.github.com>
2019-07-30 13:02:42 +0800

    added psa vessel ref

--- models/PSAQuayCraneSequence.js
+++ models/PSAQuayCraneSequence.js
@@ -5,7 +5,8 @@ const schema = new Schema({
     berthingSequence: {type: Number, default: 1},
     psaVoyage: {type: String, default: ''},
     seqTimeFrom: {type: Date, default: null},
-    seqTimeTo: {type: Date, default: null}
+    seqTimeTo: {type: Date, default: null},
+    psaVessel: {type: Schema.Types.ObjectId, ref: 'psaVessels'}
 });
 
 module.exports = mongoose.model('psaQuayCraneSequences', schema);
