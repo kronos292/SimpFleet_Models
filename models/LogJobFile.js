9e57301aaf8477a89135862a24ce43fad9395d26
kronos292 <kronos292@users.noreply.github.com>
2019-07-31 13:16:19 +0800

    added job file for logistics platform

--- models/LogJobFile.js
+++ models/LogJobFile.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const jobFileSchema = new Schema({
+const schema = new Schema({
     job: {type: Schema.Types.ObjectId, ref: 'jobs'},
     user: {type: Schema.Types.ObjectId, ref: 'users'},
     fileURL: {type: String, default: ''},
     timeUploaded: {type: Date, default: new Date()},
     version: {type: Number, default: 1},
     type: {type: String, default: ''},
-    filename: {type: String, default: ''},
-    numCopies: {type: Number, default: 1},
-    requirements: [{type: Schema.Types.Mixed}],
     remarks: {type: String, default: ''},
     isDeleted: {type: Boolean, default: false}
 });
 
-module.exports = mongoose.model('jobFiles', jobFileSchema);
+module.exports = mongoose.model('logJobFiles', schema);
