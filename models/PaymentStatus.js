0c1251b72a534d87b27f2ba0f63e1564893d33b3
Justus Soh <justus.soh@hotmail.com>
2019-07-31 17:20:42 +0800

    Added User Attribute to Payment Status

--- models/PaymentStatus.js
+++ models/PaymentStatus.js
@@ -5,7 +5,8 @@ const paymentStatusSchema = new Schema({
     index: {type: Number, default: -1},
     timestamp: {type: Date, default: new Date()},
     label: {type: String, default: ''},
-    job: {type: Schema.Types.ObjectId, ref: 'jobs'}
+    job: {type: Schema.Types.ObjectId, ref: 'jobs'},
+    user: {type: Schema.Types.ObjectId, ref: 'users'},
 });
 
 module.exports = mongoose.model('paymentStatus', paymentStatusSchema);
