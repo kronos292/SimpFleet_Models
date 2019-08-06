d2c0e8ff8be921a9086b4dcd3b07b015b4345274
kronos292 <kronos292@users.noreply.github.com>
2019-08-06 17:55:19 +0800

    added marine traffic vessel ref in vessel model

--- models/Vessel.js
+++ models/Vessel.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const vesselSchema = new Schema({
     psaVessels: [{type: Schema.Types.ObjectId, ref: 'psaVessels'}],
     mpaVessel: {type: Schema.Types.ObjectId, ref: 'mpaVessels'},
+    marineTrafficVessel: {type: Schema.Types.ObjectId, ref: 'marineTrafficVessels'},
     vesselIMOID: {type: String, default: ""},
     vesselName: {type: String, default: ""},
     vesselCallsign: {type: String, default: ""}
