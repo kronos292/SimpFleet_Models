f8544f91ebc000387146e6734bae2c3c34b48203
kronos292 <kronos292@users.noreply.github.com>
2019-08-15 10:10:21 +0800

    temporaily removed user methods

--- models/User.js
+++ models/User.js
@@ -19,15 +19,15 @@ const userSchema = new Schema({
     resetPasswordExpiry: {type: String, default: ""}
 });
 
-// generating a hash
-userSchema.methods.generateHash = function (password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
-};
+// // generating a hash
+// userSchema.methods.generateHash = function (password) {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// };
+//
+// // checking if password is valid
+// userSchema.methods.validPassword = function (password) {
+//     return bcrypt.compareSync(password, this.password);
+// };
 
 // Generate JWT
 // userSchema.methods.generateAuthToken = function() {
