43410b0301a994935cf9e932fe685d35c34ef5ee
kronos292 <kronos292@users.noreply.github.com>
2019-08-26 21:19:10 +0800

    added ETB and ETU to PSA QC

--- models/PSAQuayCraneSequence.js
+++ models/PSAQuayCraneSequence.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 const schema = new Schema({
     berthingSequence: {type: Number, default: 1},
     psaVoyage: {type: String, default: ''},
+    ETB: {type: Date, default: null},
+    ETU: {type: Date, default: null},
     seqTimeFrom: {type: Date, default: null},
     seqTimeTo: {type: Date, default: null}
 });
