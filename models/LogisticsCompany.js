00906fc8ce12c8bc882e88ea0efdec46820058ec
kronos292 <kronos292@users.noreply.github.com>
2019-09-06 15:13:02 +0800

    updated logistics company with telegram chat id

--- models/LogisticsCompany.js
+++ models/LogisticsCompany.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
     name: {type: String, default: ""},
-    location: {type: Schema.Types.ObjectId, ref: 'locations'}
+    location: {type: Schema.Types.ObjectId, ref: 'locations'},
+    telegramGroupChatId: {type: String, default: ""}
 });
 
 module.exports = mongoose.model('logisticsCompanies', schema);
