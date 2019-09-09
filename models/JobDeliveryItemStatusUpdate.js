f5b47cdcce2e1ef0cad60ead8a533dee80a6827e
kronos292 <kronos292@users.noreply.github.com>
2019-09-10 00:16:48 +0800

    Create JobDeliveryItemStatusUpdate.js

---
+++ models/JobDeliveryItemStatusUpdate.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'users'},
+    job: {type: Schema.Types.ObjectId, ref: 'jobs'},
+    verificationUploads: [{type: Schema.Types.ObjectId, ref: 'logJobFiles'}],
+    timeUpdated: {type: Date, default: new Date()}
+});
+
+module.exports = mongoose.model('jobDeliveryItemStatusUpdates', schema);
