42b55b9933e9cf34ad09c8db52f3ba37b9df3153
kronos292 <kronos292@users.noreply.github.com>
2019-09-16 17:21:07 +0800

    added id for location

--- models/Location.js
+++ models/Location.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
+    id: {type: String, default: ''},
     name: {type: String, default: ''},
     lng: {type: Number, default: 0},
     lat: {type: Number, default: 0}
