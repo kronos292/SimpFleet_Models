c7188759a8d1b9d06ef3f6f4588d1e7f1224adf0
kronos292 <kronos292@users.noreply.github.com>
2019-09-24 16:12:33 +0800

    added correspondence emails for logsitics company

--- models/LogisticsCompany.js
+++ models/LogisticsCompany.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const schema = new Schema({
     name: {type: String, default: ""},
     location: {type: Schema.Types.ObjectId, ref: 'locations'},
-    telegramGroupChatId: {type: String, default: ""}
+    telegramGroupChatId: {type: String, default: ""},
+    correspondenceEmails: [{type: String, default: ""}]
 });
 
 module.exports = mongoose.model('logisticsCompanies', schema);
