68d9ddab646c8f17169b676fe6873006e7eeb583
Justus Soh <justus.soh@hotmail.com>
2019-09-27 15:05:40 +0800

    Updated JobTrips

--- models/JobTrip.js
+++ models/JobTrip.js
@@ -4,8 +4,8 @@ const {Schema} = mongoose;
 const schema = new Schema({
     jobTripSequences: [{type: Schema.Types.ObjectId, ref: 'jobTripSequences'}],
     jobs: [{type: Schema.Types.ObjectId, ref: 'jobs'}],
-    leaveWarehouse: {type: Date, default: null},
-    endWork: {type: Date, default: null},
+    startTrip: {type: Date, default: null},
+    endTrip: {type: Date, default: null},
     driver: {type: Schema.Types.ObjectId, ref: 'drivers'},
     id: {type: String, default: ''}
 });
