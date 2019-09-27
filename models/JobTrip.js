8ea3ee56c9a8fddbf516e0e81fc42b50a212d020
Justus Soh <justus.soh@hotmail.com>
2019-09-27 12:00:13 +0800

    Updated Job Trips to include driver information

--- models/JobTrip.js
+++ models/JobTrip.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
 const schema = new Schema({
     jobTripSequences: [{type: Schema.Types.ObjectId, ref: 'jobTripSequences'}],
     jobs: [{type: Schema.Types.ObjectId, ref: 'jobs'}],
+    leaveWarehouse: {type: Date, default: null},
+    endWork: {type: Date, default: null},
+    driver: {type: Schema.Types.ObjectId, ref: 'drivers'},
     id: {type: String, default: ''}
 });
 
