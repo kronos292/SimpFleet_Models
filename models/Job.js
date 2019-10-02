4f99d28538a2a84a06781de0cbd9d4f5b4e25092
Justus Soh <justus.soh@hotmail.com>
2019-10-03 00:02:35 +0800

    Added pickupLocation to Jobs

--- models/Job.js
+++ models/Job.js
@@ -22,6 +22,8 @@ const jobSchema = new Schema({
     isCancelled: {type: String, default: 'Nil'},
     isArchived: {type: Boolean, default: false},
     createDSA: {type: Boolean, default: false},
+    pickup: {type: Boolean, default: false},
+    pickupLocation: [{type: Schema.Types.ObjectId, ref: 'pickupLocations'}],
     vesselLighterName: {type: String, default: ''},
     vesselLighterLocation: {type: String, default: ''},
     vesselLighterRemarks: {type: String, default: ''},
