5bb36beb05e5c490f81995142091293ca61a2a7d
kronos292 <kronos292@users.noreply.github.com>
2019-10-16 22:25:38 +0800

    Create JobItemPricing.js

---
+++ models/JobItemPricing.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    price: {type: Number, default: 0},
+    uom: {type: String, default: ''}
+});
+
+module.exports = mongoose.model('jobItemPricings', schema);
