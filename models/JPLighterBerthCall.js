32b00c7013a47f749db4ee2bbdf2761648ec280d
kronos292 <kronos292@users.noreply.github.com>
2019-10-23 21:42:26 +0800

    added company name

--- models/JPLighterBerthCall.js
+++ models/JPLighterBerthCall.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const schema = new Schema({
     lighterName: {type: String, default: ''},
     lighterNumber: {type: String, default: ''},
+    companyName: {type: String, default: ''},
     crane: {type: String, default: ''},
     arrivalDateTime: {type: Date, default: null},
     departureDateTime: {type: Date, default: null},
