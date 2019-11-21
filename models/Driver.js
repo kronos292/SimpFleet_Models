ba1e3e6d058ed7bae749b3896264492d0584bfce
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-11-21 23:35:26 +0700

    update driver schema

--- models/Driver.js
+++ models/Driver.js
@@ -1,12 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    name: {type: String, default: ''},
-    contact: {type: String, default: ''},
-    email: {type: String, default: ''},
-    company: {type: String, default: ''}
+  username: {
+    type: String,
+    trim: true,
+    required: true,
 
+    default: ""
+  },
+  phonenumber: {
+    type: String,
+    default: "",
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    default: "",
+    trim: true,
+    required: true
+  },
+  company: {
+    type: String,
+    unique: true,
+    default: "",
+    trim: true,
+    required: true
+  },
+  password: {
+    type: String,
+    default: "",
+    trim: true,
+    required: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
-module.exports = mongoose.model('drivers', schema);
+// generating a hash
+schema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+schema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model("drivers", schema);
