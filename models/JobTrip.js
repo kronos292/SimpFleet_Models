43e0cfa71994e09489baa0f29a202e58deb5c6ed
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-12-23 15:55:52 +0700

    change ref driver to transportUsers on jobTrips schema

--- models/JobTrip.js
+++ models/JobTrip.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const schema = new Schema({
-    jobTripSequences: [{type: Schema.Types.ObjectId, ref: 'jobTripSequences'}],
-    jobs: [{type: Schema.Types.ObjectId, ref: 'jobs'}],
-    startTrip: {type: Date, default: null},
-    endTrip: {type: Date, default: null},
-    driver: {type: Schema.Types.ObjectId, ref: 'drivers'},
-    id: {type: String, default: ''}
+  jobTripSequences: [{ type: Schema.Types.ObjectId, ref: "jobTripSequences" }],
+  jobs: [{ type: Schema.Types.ObjectId, ref: "jobs" }],
+  startTrip: { type: Date, default: null },
+  endTrip: { type: Date, default: null },
+  driver: { type: Schema.Types.ObjectId, ref: "transportUsers" },
+  id: { type: String, default: "" }
 });
 
-module.exports = mongoose.model('jobTrips', schema);
+module.exports = mongoose.model("jobTrips", schema);
