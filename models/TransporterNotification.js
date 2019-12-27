4fe412f22c51a2f4478f2b1725ffd479188cbc60
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-12-27 11:06:18 +0700

    add message field to tranporterNotification

--- models/TransporterNotification.js
+++ models/TransporterNotification.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const schema = new Schema({
-	user: { type: Schema.Types.ObjectId, ref: "users" },
-	job: { type: Schema.Types.ObjectId, ref: "jobs" },
-	callTime: { type: Date, default: new Date() },
-	type: { type: String, default: "" },
-	isEmail: { type: Boolean, default: false },
-	isMobile: { type: Boolean, default: false },
-	isActive: { type: Boolean, default: false },
-	isNotified: { type: Boolean, default: false },
-	timestamp: { type: Date, default: new Date() }
+  user: { type: Schema.Types.ObjectId, ref: "users" },
+  job: { type: Schema.Types.ObjectId, ref: "jobs" },
+  message: { type: String, default: "" },
+  callTime: { type: Date, default: new Date() },
+  type: { type: String, default: "" },
+  isEmail: { type: Boolean, default: false },
+  isMobile: { type: Boolean, default: false },
+  isActive: { type: Boolean, default: false },
+  isNotified: { type: Boolean, default: false },
+  timestamp: { type: Date, default: new Date() }
 });
 
 module.exports = mongoose.model("transporterNotifications", schema);
