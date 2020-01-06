c33919961d5267005fc617ca8e38bbf9c3e5644d
unknown <justin_mendoza5@yahoo.com>
2020-01-06 17:22:57 +0800

    Add checklist for job status

--- models/JobTracker.js
+++ models/JobTracker.js
@@ -2,20 +2,24 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const jobTrackerSchema = new Schema({
-    index: { type: Number, default: -1 },
-    timestamp: { type: Date, default: new Date() },
-    trackingType: { type: String, default: "" },
-    title: { type: String, default: "" },
-    description: { type: String, default: "" },
-    remarks: { type: String, default: "" },
-    job: { type: Schema.Types.ObjectId, ref: "jobs" },
-    isDocumentRequired: { type: Boolean, default: false },
-    jobTrackerSuppliers: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "jobTrackerSuppliers"
-        }
-    ]
+	index: { type: Number, default: -1 },
+	timestamp: { type: Date, default: new Date() },
+	trackingType: { type: String, default: "" },
+	title: { type: String, default: "" },
+	description: { type: String, default: "" },
+	remarks: { type: String, default: "" },
+	job: { type: Schema.Types.ObjectId, ref: "jobs" },
+	isDocumentRequired: { type: Boolean, default: false },
+	jobTrackerSuppliers: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "jobTrackerSuppliers"
+		}
+	],
+	jobTrackerItemCheckLists: {
+		type: Schema.Types.ObjectId,
+		ref: "jobTrackerItemCheckLists"
+	}
 });
 
 module.exports = mongoose.model("jobTrackers", jobTrackerSchema);
