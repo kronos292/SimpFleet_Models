1785418429caa4267af53ae008ac395941ca9c87
kronos292 <kronos292@users.noreply.github.com>
2020-01-07 12:18:10 +0800

    Updated Vessel Tracker schema

--- models/VesselTracker.js
+++ models/VesselTracker.js
@@ -3,11 +3,10 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
     vessel: {type: Schema.Types.ObjectId, ref: 'vessels'},
-    isEmail: {type: Boolean, default: false},
-    isSMS: {type: Boolean, default: false},
-    isMobile: {type: Boolean, default: false},
-    isActive: {type: Boolean, default: true},
-    job: {type: Schema.Types.ObjectId, ref: 'jobs'}
+    emailNotification: {type: Boolean, default: false},
+    smsNotification: {type: Boolean, default: false},
+    mobileNotification: {type: Boolean, default: false},
+    isActive: {type: Boolean, default: true}
 });
 
 module.exports = mongoose.model('vesselTrackers', schema);
