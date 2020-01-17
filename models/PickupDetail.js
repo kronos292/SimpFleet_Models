5e4dbc44e72a0f8cee2a9e2929ee508061b8acd7
unknown <justin_mendoza5@yahoo.com>
2020-01-17 15:26:12 +0800

    add pickupStatus field in pickup details schema

--- models/PickupDetail.js
+++ models/PickupDetail.js
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const schema = new Schema({
     pickupDateTime: {type: Date, default: null},
     pickupLocation: {type: Schema.Types.ObjectId, ref: 'pickupLocations'},
+    pickupStatus : {type: String, default: 'open'},
 });
 
 module.exports = mongoose.model('pickupDetails', schema);
