33309c46e84af255990356a5286aa1bbd48cd44e
kronos292 <kronos292@users.noreply.github.com>
2020-01-31 22:00:19 +0800

    removed previous pricing schema

--- models/JobItemPriceIndex.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const schema = new Schema({
-    index:{type: String, default: ''},
-    price: {type: Number, default: 0},
-});
-
-module.exports = mongoose.model('jobItemPriceIndexes', schema);
