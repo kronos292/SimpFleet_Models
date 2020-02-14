46e75241f70735cc69ff0fe478ad3ca4d114f5c8
kronos292 <kronos292@users.noreply.github.com>
2020-02-14 18:08:25 +0800

    added estimated and actual price breakdown for jobs

--- models/Job.js
+++ models/Job.js
@@ -38,7 +38,8 @@ const jobSchema = new Schema({
     psaVoyageNumberOut: {type: String, default: ""},
     psaBerf: {type: String, default: ""},
     telegramMessageId: {type: String, default: ""},
-    jobPrices:[{type: Schema.Types.ObjectId, ref: 'jobPrices'}],
+    estimatedJobPriceBreakdown: [{type: Schema.Types.Mixed, default: {}}],
+    actualJobPriceBreakdown: [{type: Schema.Types.Mixed, default: {}}],
     isDeleted: {type: Boolean, default: false} // For jobs that are accidentally created or for testing, and need to be removed from actual job count.
 });
 
