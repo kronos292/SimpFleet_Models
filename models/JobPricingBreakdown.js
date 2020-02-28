527c769078f69360789c4823625dda82895b3047
kronos292 <kronos292@users.noreply.github.com>
2020-02-28 18:55:39 +0800

    Update JobPricingBreakdown.js

--- models/JobPricingBreakdown.js
+++ models/JobPricingBreakdown.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
+    id: {type: String, default: ''},
     name: {type: String, default: ''},
     description: {type: String, default: ''},
     price: {type: Number, default: 0},
     type: {type: String, default: ''}, // Estimated, Actual
-    job: {type: Schema.Types.ObjectId, ref: "jobs"}
+    job: {type: Schema.Types.ObjectId, ref: "jobs"},
+    dateTimeCreated: {type: Date, default: new Date()}, // Date and time created
+    dateTimeUpdated: {type: Date, default: new Date()} // Date and time updated
 });
 
 module.exports = mongoose.model('jobPricingBreakdowns', schema);
