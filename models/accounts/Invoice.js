8ff1f7d58dd06efbe9bd0208667774829551601b
kronos292 <kronos292@users.noreply.github.com>
2020-03-03 22:07:12 +0800

    added datetime variables to invoice

--- models/accounts/Invoice.js
+++ models/accounts/Invoice.js
@@ -3,9 +3,12 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
     dateTimeCreated: {type: Date, default: new Date()},
+    dateTimeIssued: {type: Date, default: new Date()},
+    dateTimeDue: {type: Date, default: new Date()},
+    dateTimePaid: {type: Date, default: new Date()},
     type: {type: String, default: ''}, // receivable, payable
     xeroId: {type: String, default: ''},
-    xeroNumber: {type: String, default: ''},
+    xeroNumber: {type: String, default: ''}
 });
 
 module.exports = mongoose.model('invoices', schema);
