30ad168ec8ad33b58fe6da04a2889f9a2d9c339c
kronos292 <kronos292@users.noreply.github.com>
2020-03-03 14:44:50 +0800

    added offland pricing computation

--- pricing/pricingMethods.js
+++ pricing/pricingMethods.js
@@ -58,6 +58,9 @@ const truckPricing = [
     }
 ];
 
+// Flat rate for offland pricing.
+const offlandPrice = 30;
+
 // Function to check if the date is a public holiday.
 async function checkPublicHoliday(date) {
     const holidays = await getHolidays({
@@ -106,13 +109,12 @@ async function getTruckPrice(job, truckPriceList) {
     return totalPrice;
 }
 
-// Function to compute total item prices
-async function computeItemPricing(job) {
-    let totalPrice  = 0;
+// Function to compute price of job items.
+async function computeJobItemPrice(job, jobItems) {
     const hourIndex = await getHourIndex(job);
-    const {jobItems} = job;
+    let totalPrice = 0;
 
-    // Determine pallet price list.
+    // Determine whether job has pickup.
     const jobHasPickup = await checkForPickup(job);
 
     // Retrieve price list via working hours.
@@ -120,7 +122,6 @@ async function computeItemPricing(job) {
     const palletPriceList = jobHasPickup? palletPricingPickup[hourIndex]: palletPricingWH[hourIndex];
     const truckPriceList = truckPricing[hourIndex];
 
-    // Compute job item pricing
     for(let i = 0; i < jobItems.length; i++) {
         const jobItem = jobItems[i];
         const {uom, quantity} = jobItem;
@@ -136,6 +137,35 @@ async function computeItemPricing(job) {
             totalPrice += await getTruckPrice(job, truckPriceList);
         }
     }
+
+    return totalPrice;
+}
+
+// Function to compute price of offland items.
+async function computeOfflandItemPrice(job) {
+    const {jobItems, jobOfflandItems} = job;
+    let totalPrice = 0;
+
+    if(jobOfflandItems && jobOfflandItems.length > 0) {
+        if(jobItems && jobItems.length > 0) {
+            totalPrice += offlandPrice;
+        } else {
+            totalPrice += computeJobItemPrice(job, jobOfflandItems);
+        }
+    }
+
+    return totalPrice;
+}
+
+// Function to compute total item prices
+async function computeItemPricing(job) {
+    let totalPrice = 0;
+
+    // Compute job item pricing.
+    totalPrice += computeJobItemPrice(job, job.jobItems);
+
+    // Compute offland item pricing.
+    totalPrice += computeOfflandItemPrice(job);
 
     return totalPrice;
 }
@@ -158,104 +188,5 @@ module.exports = {
                 price: itemPricing
             }
         ];
-    },
-    calculateDeliveryPricing: async (jobItems) => {
-        let total = 0;
-        // for (let i = 0; i < jobItems.length; i++) {
-        //     const jobItem = jobItems[i];
-        //     const {uom, quantity} = jobItem;
-        //     let filteredUOM = uom;
-        //     if (uom === 'Bundle' || uom === 'Blue Bins') {
-        //         filteredUOM = 'Pallet';
-        //     }
-        //     const jobDeliveryItemPricings = await JobDeliveryItemPricing.find({uom: filteredUOM, type: 'working'});
-        //     for (let j = 0; j < jobDeliveryItemPricings.length; j++) {
-        //         const jobDeliveryItemPricing = jobDeliveryItemPricings[j];
-        //         if (jobDeliveryItemPricing.quantityLow <= quantity && jobDeliveryItemPricing.quantityHigh >= quantity) {
-        //             total += jobDeliveryItemPricing.price;
-        //             break;
-        //         }
-        //     }
-        // }
-        return total;
-    },
-
-    indexJobItemPricing: async (jobItem, jobObj, type) => {
-        // const job = await Job.findOne({_id: jobObj._id}).populate({
-        //     path: 'vesselLoadingLocation',
-        //     model: 'vesselLoadingLocations'
-        // }).populate({
-        //     path: 'pickupDetails',
-        //     model: 'pickupDetails',
-        //     populate: [
-        //         {
-        //             path: 'pickupLocation',
-        //             model: 'pickupLocations'
-        //         }
-        //     ]
-        // }).select();
-        //
-        // let serial = '';
-        // if (type === 'Delivery') {
-        //     serial += 'D';
-        //     let deliveryTime;
-        //     if (job.vesselLoadingLocation.type === 'port' && job.psaBerthingDateTime) {
-        //         //for now use berthing time, eventually might need change to delivery time
-        //         deliveryTime = job.psaBerthingDateTime;
-        //     } else if (job.vesselLoadingDateTime) {
-        //         deliveryTime = job.vesselLoadingDateTime
-        //     } else {
-        //         //if no time return 0 price
-        //         console.log("No loading Time");
-        //         return 0;
-        //     }
-        //     deliveryTime = moment(deliveryTime);
-        //     let workingHours = false;
-        //     if (deliveryTime.hours() > 8 && deliveryTime.hours() <17){
-        //         workingHours = true;
-        //     }else if( deliveryTime.hours() === 8 && deliveryTime.minutes() >= 30){
-        //         workingHours = true;
-        //     }else if(deliveryTime.hours() === 17 && deliveryTime.minutes() <= 30){
-        //         workingHours = true;
-        //     }
-        //     if (workingHours && deliveryTime.isoWeekday() <= 6) {
-        //         serial += "WH"
-        //     } else {
-        //         serial += "NWH"
-        //     }
-        // } else if (type === 'Collection') {
-        //     serial += 'C';
-        //     serial += "WH";
-        // }
-        // let quantity = jobItem.quantity;
-        // if (jobItem.uom === 'Pallet') {
-        //     if (quantity <= 5) {
-        //         serial += quantity + 'P';
-        //     } else {
-        //         serial += "6P"
-        //     }
-        // }
-        // let jobItemPriceIndex = await JobItemPriceIndex.findOne({index: serial}).select();
-        // if (!jobItemPriceIndex) {
-        //     console.log(serial + " index does not exist");
-        //     return 0;
-        // }
-        // console.log(serial + " found @ " + jobItemPriceIndex.price);
-        return 0;
-    },
-    getOfflandingPrice: async (uom) => {
-        // let serial = 'O';
-        // if (uom === 'Pallet') {
-        //     serial += 'P'
-        // } else if (uom === 'Import Permit') {
-        //     serial += 'IP'
-        // }
-        // let jobItemPriceIndex = await JobItemPriceIndex.findOne({index: serial}).select();
-        // if (!jobItemPriceIndex) {
-        //     console.log(serial + " index does not exist");
-        //     return 0;
-        // }
-        // console.log(serial + " found @ " + jobItemPriceIndex.price);
-        return 0;
     }
 };
