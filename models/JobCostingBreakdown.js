41f311cc82f1cc8a7bd241eded80331a3b57a012
kronos292 <kronos292@users.noreply.github.com>
2020-03-12 17:51:09 +0800

    Update JobCostingBreakdown.js

--- models/JobCostingBreakdown.js
+++ models/JobCostingBreakdown.js
@@ -5,7 +5,7 @@ const schema = new Schema({
     id: {type: String, default: ''},
     name: {type: String, default: ''},
     description: {type: String, default: ''},
-    price: {type: Number, default: 0},
+    cost: {type: Number, default: 0},
     type: {type: String, default: ''}, // Estimated, Actual
     jobTrip: {type: Schema.Types.ObjectId, ref: "jobTrips"},
     dateTimeCreated: {type: Date, default: new Date()}, // Date and time created
