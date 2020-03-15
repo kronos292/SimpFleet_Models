b7fea7f3a0b13c1b96293ea1234367d03b4f1c71
kronos292 <kronos292@users.noreply.github.com>
2020-03-15 20:51:50 +0800

    added textract id

--- models/accounts/PaymentInvoiceUpload.js
+++ models/accounts/PaymentInvoiceUpload.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const schema = new Schema({
     fileURL: {type: String, default: ''},
     status: {type: String, default: 'Pending'}, // Pending, Validated, Rejected
-    jobPayment: {type: Schema.Types.ObjectId, ref: 'jobPayments'}
+    jobPayment: {type: Schema.Types.ObjectId, ref: 'jobPayments'},
+    textractJobId: {type: String, default: ''}
 });
 
 module.exports = mongoose.model('paymentInvoiceUploads', schema);
