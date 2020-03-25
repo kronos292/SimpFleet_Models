a74d18bbe1a5b094d7aa1f278f948ff7e9d86991
kronos292 <kronos292@users.noreply.github.com>
2020-03-25 19:17:47 +0800

    added telegram group chat id for user companies

--- models/UserCompany.js
+++ models/UserCompany.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const schema = new Schema({
     name: {type: String, default: ""},
     location: {type: Schema.Types.ObjectId, ref: 'locations'},
-    companyAccountContact: {type: Schema.Types.ObjectId, ref: 'companyAccountContacts'}
+    companyAccountContact: {type: Schema.Types.ObjectId, ref: 'companyAccountContacts'},
+    telegramGroupChatId: {type: String, default: ""}
 });
 
 module.exports = mongoose.model('userCompanies', schema);
