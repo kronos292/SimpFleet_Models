4f8b87bbb4109d5e9302cf0f22a399c6770ca412
kronos292 <kronos292@users.noreply.github.com>
2020-04-06 22:18:57 +0800

    Update Vessel.js

--- models/Vessel.js
+++ models/Vessel.js
@@ -6,6 +6,7 @@ const vesselSchema = new Schema({
     psaVessel: {type: Schema.Types.ObjectId, ref: 'psaVessels'}, // Latest PSA Vessel
     mpaVessel: {type: Schema.Types.ObjectId, ref: 'mpaVessels'},
     marineTrafficVessel: {type: Schema.Types.ObjectId, ref: 'marineTrafficVessels'},
+    psaQuayCraneSequence: {type: Schema.Types.ObjectId, ref: 'psaQuayCraneSequences'},  // Latest PSA QC Sequence
     vesselIMOID: {type: String, default: ""},
     vesselName: {type: String, default: ""},
     vesselCallsign: {type: String, default: ""}
