96d17c36ed728a7e1984b79c42e0b4fcbb522c9e
kronos292 <kronos292@users.noreply.github.com>
2020-04-28 13:31:55 +0800

    added more vessel details to schema

--- models/Vessel.js
+++ models/Vessel.js
@@ -7,9 +7,25 @@ const vesselSchema = new Schema({
     mpaVessel: {type: Schema.Types.ObjectId, ref: 'mpaVessels'},
     marineTrafficVessel: {type: Schema.Types.ObjectId, ref: 'marineTrafficVessels'},
     psaQuayCraneSequence: {type: Schema.Types.ObjectId, ref: 'psaQuayCraneSequences'},  // Latest PSA QC Sequence
+
+    // Vessel details from MPA sources
     vesselIMOID: {type: String, default: ""},
     vesselName: {type: String, default: ""},
-    vesselCallsign: {type: String, default: ""}
+    vesselCallsign: {type: String, default: ""},
+    vesselFlag: {type: String, default: ""},
+    vesselLength: {type: String, default: ""},
+    vesselBreadth: {type: String, default: ""},
+    vesselDepth: {type: String, default: ""},
+    vesselType: {type: String, default: ""},
+    vesselGrossTonnage: {type: String, default: ""},
+    vesselNetTonnage: {type: String, default: ""},
+    vesselDeadWeight: {type: String, default: ""},
+    vesselMMSINumber: {type: String, default: ""},
+    vesselYearBuilt: {type: String, default: ""},
+    vesselIsmManager: {type: String, default: ""},
+    vesselShipManager: {type: String, default: ""},
+    vesselRegisteredOwnership: {type: String, default: ""},
+    vesselClassificationSociety: {type: String, default: ""},
 });
 
 module.exports = mongoose.model('vessels', vesselSchema);
