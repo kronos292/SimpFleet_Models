3f3956b20a176a5fe0c20ad731c081171d7caadb
kronos292 <kronos292@users.noreply.github.com>
2020-05-09 18:33:45 +0800

    Update VesselLoadingLocation.js

--- models/VesselLoadingLocation.js
+++ models/VesselLoadingLocation.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const schema = new Schema({
     name: {type: String, default: ''},
     type: {type: String, default: ''},
+    aliases: [{type: String, default: ''}],
     location: {type: Schema.Types.ObjectId, ref: 'locations'}
 });
 
