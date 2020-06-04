cb666290f3d242237934b1202c58789e2b3b5d21
kronos292 <kronos292@users.noreply.github.com>
2020-06-04 19:17:48 +0800

    Update lighterBoatController.js

--- controllers/lighterBoatController.js
+++ controllers/lighterBoatController.js
@@ -1,7 +1,10 @@
 const {LighterBoat} = require('../util/models');
 
 async function find(findMethod, params) {
-    return await LighterBoat[findMethod](params).select();
+    return await LighterBoat[findMethod](params).populate({
+        path: 'lighterBoatCompany',
+        model: 'lighterBoatCompanies'
+    }).select();
 }
 
 module.exports = {
