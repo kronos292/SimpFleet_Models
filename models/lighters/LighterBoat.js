758a727113f803ad2498e0d1cc5d55612a371590
kronos292 <kronos292@users.noreply.github.com>
2020-06-04 18:29:43 +0800

    Update LighterBoat.js

--- models/lighters/LighterBoat.js
+++ models/lighters/LighterBoat.js
@@ -5,7 +5,8 @@ const schema = new Schema({
     lighterName: {type: String, default: ''},
     lighterNumber: {type: String, default: ''},
     lighterIMO: {type: String, default: ''},
-    lighterBoatCompany: {type: Schema.Types.ObjectId, ref: 'lighterBoatCompanies'}
+    lighterBoatCompany: {type: Schema.Types.ObjectId, ref: 'lighterBoatCompanies'},
+    status: {type: String, default: ''} // ACTIVE, INACTIVE
 });
 
 module.exports = mongoose.model('lighterBoats', schema);
