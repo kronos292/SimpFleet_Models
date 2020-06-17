453d25bcab353d952025970e1a04978e4aacabbb
kronos292 <kronos292@users.noreply.github.com>
2020-06-17 16:15:24 +0800

    Update TransporterFile.js

--- models/TransporterFile.js
+++ models/TransporterFile.js
@@ -2,34 +2,18 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const schema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "transportUsers"
-  },
-  fileURL: {
-    type: String,
-    default: ""
-  },
-  filename: {
-    type: String,
-    default: ""
-  },
-  fileType: {
-    type: String,
-    default: ""
-  },
-  fileFor: {
-    type: String,
-    default: ""
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false
-  },
-  timeUploaded: {
-    type: Date,
-    default: new Date()
-  }
+  job: {type: Schema.Types.ObjectId, ref: 'jobs'},
+  logisticsUser: {type: Schema.Types.ObjectId, ref: 'logisticsUsers'}, // Null if Transport user uploads the file
+  transportUser: {type: Schema.Types.ObjectId, ref: 'users'}, // Null if Logistics user uploads the file
+  fileURL: {type: String, default: ''},
+  timeUploaded: {type: Date, default: new Date()},
+  version: {type: Number, default: 1},
+  type: {type: String, default: ''},
+  filename: {type: String, default: ''},
+  numCopies: {type: Number, default: 1},
+  requirements: [{type: Schema.Types.Mixed}],
+  remarks: {type: String, default: ''},
+  isDeleted: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model("transporterFile", schema);
