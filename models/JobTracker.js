b670981778e33eb2403bafe1df1ded3cb65cf186
kronos292 <kronos292@users.noreply.github.com>
2020-07-09 12:47:18 +0800

    added enhanced status attributes

--- models/JobTracker.js
+++ models/JobTracker.js
@@ -4,11 +4,16 @@ const { Schema } = mongoose;
 const jobTrackerSchema = new Schema({
 	index: { type: Number, default: -1 },
 	timestamp: { type: Date, default: new Date() },
-	trackingType: { type: String, default: "" },
+	trackingType: { type: String, default: "" }, // For icon display.
 	title: { type: String, default: "" },
 	description: { type: String, default: "" },
 	remarks: { type: String, default: "" },
 	job: { type: Schema.Types.ObjectId, ref: "jobs" },
+	transporterFiles: [{ type: Schema.Types.ObjectId, ref: "transporterFile" }], // Files uploaded during the various stages of the trip.
+	userType: { type: String, default: "" }, // JOB_OWNER, ADMIN, TRANSPORTER
+	location: { type: Schema.Types.ObjectId, ref: "locations" }, // Location of this tracker.
+	type: { type: String, default: "" }, // START, ONGOING, END
+	isCompleted: { type: Boolean, default: false }, // Whether the tracker is completed currently.
 	isDocumentRequired: { type: Boolean, default: false },
 	jobTrackerSuppliers: [
 		{
