ef74775f4274587cbcfcf5f1f1d2f64a3aad8d8f
Justin Mendoza <6860757+justindcmendoza@users.noreply.github.com>
2021-02-01 13:27:06 +0800

    Update Location Model

--- models/Location.js
+++ models/Location.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    id: {type: String, default: ''},
-    name: {type: String, default: ''},
-    lng: {type: Number, default: 0},
-    lat: {type: Number, default: 0}
+    id: { type: String, default: "" },
+    name: { type: String, default: "" },
+    coords: {
+        lng: { type: Number, default: 0 },
+        lat: { type: Number, default: 0 },
+    },
+    companyDetails: { type: String, default: "" },
+    buildingDetails: { type: String, default: "" },
+    user: { type: Schema.Types.ObjectId, ref: "users" },
 });
 
-module.exports = mongoose.model('locations', schema);
+module.exports = mongoose.model("locations", schema);
