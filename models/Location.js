4d641e63ff111e9853bc15030b79e2f24ff9394d
Justin Mendoza <6860757+justindcmendoza@users.noreply.github.com>
2021-02-15 17:42:32 +0800

    Update Locations schema

--- models/Location.js
+++ models/Location.js
@@ -3,13 +3,15 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
     id: { type: String, default: "" },
-    name: { type: String, default: "" },
     coords: {
         lng: { type: Number, default: 0 },
         lat: { type: Number, default: 0 },
     },
-    companyDetails: { type: String, default: "" },
-    buildingDetails: { type: String, default: "" },
+    name: { type: String, default: "" },
+    address: { type: String, default: "" },
+    addressLine2: { type: String, default: "" },
+    timestamp: {type: Date, default: new Date()},
+    type: {type: String, default: ''},
     user: { type: Schema.Types.ObjectId, ref: "users" },
 });
 
